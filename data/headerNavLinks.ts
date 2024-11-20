60754a4e9245db7de9dc03f3b59bd0294492d3ed
云微 <1067852565@qq.com>
2024-11-20 02:21:49 +0000

    fix

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: '主页' },
   { href: '/projects', title: '档案库' },
-  { href: '/search', title: '档案检索' },
+  { href: '/search', title: '检索' },
   { href: '/tags', title: '发现' },
   { href: '/blog', title: '博客' },
   { href: '/add', title: '添加' },
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,4 +1,5 @@
-import digitalYml from '../digital.yml'
+// Import the JSON data
+import independenceRepo from '../independence_repo.json';
 
 interface Project {
   title: string
@@ -7,11 +8,14 @@ interface Project {
   imgSrc?: string
 }
 
-const projectsData: Project[] = digitalYml.independence.map((repo) => ({
+const projectsData: Project[] = independenceRepo.map((repo) => ({
   title: repo.name,
   description: repo.description,
   imgSrc: '/static/images/time-machine.jpg',
   href: repo.url,
 }))
+
+// Use the data
+console.log(independenceRepo);
 
 export default projectsData
