967d40f3dfe82d38b77e22ee07ae2c2a3529c6e3
云微 <1067852565@qq.com>
2024-11-22 02:06:08 +0000

    remove blogs

--- app/Main.tsx
+++ app/Main.tsx
@@ -11,9 +11,9 @@ export default function Home({ posts }) {
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="dark:prose-dark prose max-w-none pb-8 pt-8">
+        {/* <div className="dark:prose-dark prose max-w-none pb-8 pt-8">
           <MarkdownFetcher url="https://raw.githubusercontent.com/transTerminus/trans-digital-cn/refs/heads/main/README.md" />
-        </div>
+        </div> */}
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Latest
