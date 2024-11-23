221e7e6e1c5741bfac97a74516b5515f3d698c81
云微 <1067852565@qq.com>
2024-11-23 03:52:37 +0000

    update

--- .github/fetch_data.sh
+++ .github/fetch_data.sh
@@ -5,5 +5,3 @@ set -e
 export OPENAI_TEMPERATURE=0.7
 
 python3 .github/scripts/toc/independence_info.py
-
-wget https://github.com/transTerminus/data-analysis/raw/refs/heads/main/index/combined_index.yml
