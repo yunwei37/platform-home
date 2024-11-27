ac6bba113697506d3b4aa21e028a3cc2672cab10
Yusheng Zheng <yunwei356@gmail.com>
2024-11-27 06:44:31 +0000

    update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteUrl: 'https://transchinese.org/',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
-  socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
+  socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.jpg`,
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'yunwei356@gmail.com',
   github: 'https://github.com/transTerminus',
