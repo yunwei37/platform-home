c30bc45eddd136124ff6b12dd1e234f8899ea814
云微 <1067852565@qq.com>
2024-12-27 01:00:57 +0900

    Update route.ts

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -48,8 +48,8 @@ function searchDocuments(index: SearchIndex, params: SearchParams): SearchResult
       if (
         (params.query === '' || // Search term match
           key.toLowerCase().includes(params.query.toLowerCase()) ||
-          document.description.toLowerCase().includes(params.query.toLowerCase()) ||
-          document.tags.some((tag) => tag.toLowerCase().includes(params.query.toLowerCase()))) &&
+          document.description.toLowerCase().includes(params.query.toLowerCase())) &&
+        (!params.tag || document.tags.includes(params.tag)) && // Tag match
         (!params.year || document.date.includes(params.year)) && // Year match
         (!params.region || document.region.toLowerCase() === params.region.toLowerCase()) // Region match
       ) {
