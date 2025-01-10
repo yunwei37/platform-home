1a070eb06893f76be1dffd7acf9b8aea6674f120
weekendfish <yusen356@gmail.com>
2025-01-10 23:37:13 +0000

    update

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,10 +1,10 @@
 const headerNavLinks = [
   { href: '/', title: '主页' },
-  { href: '/projects', title: '档案库' },
+  { href: '/projects', title: '档案馆' },
   { href: '/search', title: '检索' },
   { href: '/discover', title: '发现' },
   { href: '/blog', title: '博客' },
-  { href: '/add', title: '添加' },
+  { href: '/add', title: '社区' },
   { href: '/about', title: '关于' },
 ]
 
