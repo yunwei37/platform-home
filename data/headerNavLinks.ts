ed835750fcca5a36f541096bd328c440cad19603
weekendfish <yusen356@gmail.com>
2025-02-18 02:10:57 +0000

    update

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: '主页' },
-  { href: '/projects', title: '档案馆' },
+  { href: 'https://docs.transchinese.org', title: '文档' },
   { href: '/search', title: '检索' },
   { href: '/discover', title: '发现' },
   { href: '/blog', title: '博客' },
